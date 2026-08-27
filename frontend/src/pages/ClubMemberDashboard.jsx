7e9e9e261297dca32e5092430f104f85dafbadd6
Abhinav kumar <abhinavananya1234@gmail.com>
2026-08-27 15:05:29 +0530

    style: increase max width of member dashboard for wider cards

--- frontend/src/pages/ClubMemberDashboard.jsx
+++ frontend/src/pages/ClubMemberDashboard.jsx
@@ -213,7 +213,7 @@ const ClubMemberDashboard = () => {
       </nav>
 
       {/* Main Content */}
-      <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         
         {/* Profile Directory */}
         {activeTab === 'profile' && (
